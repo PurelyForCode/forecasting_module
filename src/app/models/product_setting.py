236e3b04b2ba822132bc664c3d526426b5fe85f1
PurelyForCode <purelyforcode@gmail.com>
2025-09-16 21:03:07 +0800

    init

--- src/app/models/product_setting.py
+++
@@ -1,9 +0,0 @@
-from typing import Literal
-
-class ProductSetting:
-    classification: Literal["slow", "fast"]
-
-    def __init__(self, id: str, product_id: str, classification: Literal["slow", "fast"], ):
-        self.id = id
-        self.product_id = product_id
-        self.classification = classification
